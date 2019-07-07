af43edf4a1fdd261c8bc10454347b5947aa7e3ad
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2019-07-07 12:20:12 +0530

    Update start-jenkins.sh

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -2,4 +2,4 @@
 
 mkdir /var/jenkins_home
 chmod 777 /var/jenkins_home
-docker run -d -p 8080:8080 -p 50000:50000 -v /opt/jenkins_home:/var/jenkins_home jenkins
+docker run -d -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home jenkins
