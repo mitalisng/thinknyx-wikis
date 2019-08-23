5f1ada7b693a8cd15a138a35ae2703299e6f7a99
root <root@ip-172-31-42-224.us-east-2.compute.internal>
2019-08-23 04:40:10 +0000

    update jenkins start script

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-mkdir /var/jenkins_home
+name='jenkins'
+
+[[ -d /var/jenkins_home ]] || mkdir /var/jenkins_home
 chmod 777 /var/jenkins_home
-docker run -d -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home jenkins/jenkins:lts
+
+[[ $(docker ps -f "name=$name" --format '{{.Names}}') == $name ]] || docker run -d -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home --name jenkins jenkins/jenkins:lts
+
